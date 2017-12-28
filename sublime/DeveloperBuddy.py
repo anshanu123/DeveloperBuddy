9ca21c5855e8be603b39463ddcc249594f7d9b92
neeasthana <neeasthana@gmail.com>
2017-12-27 20:54:27 -0500

    server working example within sublime

--- sublime/DeveloperBuddy.py
+++ sublime/DeveloperBuddy.py
@@ -1,18 +1,20 @@
 import sublime, sublime_plugin
-from flask import Flask, request, redirect
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from threading import Thread
+#from flask import Flask, request, redirect
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
 def commentLineHandler(request_form):
-	line = request.form['line']
-	print("got here")
-	#sublime edit line
+    line = request.form['line']
+    print("got here")
+    #sublime edit line
 
 processing_dict = {
-	"commentLine": {"params":["line"], "callback":commentLineHandler}
+    "commentLine": {"params":["line"], "callback":commentLineHandler}
 }
 
-@app.route('/alexa', methods=['POST'])
+#@app.route('/alexa', methods=['POST'])
 def alexa():
     """processing handle for alexa app"""
     command = request.form['command'] #ex. comment, delete
@@ -26,10 +28,53 @@ def alexa():
 
 
 #class ExampleCommand(sublime_plugin.TextCommand):
-#	def run(self, edit):
-#		self.view.insert(edit, 0, "Hello, World!")
+#   def run(self, edit):
+#       self.view.insert(edit, 0, "Hello, World!")
+
+class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+ 
+  # GET
+  def do_GET(self):
+        # Send response status code
+        self.send_response(200)
+ 
+        # Send headers
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+ 
+        # Send message back to client
+        message = "Hello world!"
+        # Write content as utf-8 data
+        self.wfile.write(bytes(message, "utf8"))
+        return
+
+def start_server():
+    server.serve_forever()
 
 def plugin_loaded():
-	"""Called at the start of the sublime plugin"""
-	print("loaded plugin")
-	app.run(host = "0.0.0.0", port=8080)
+    """Called at the start of the sublime plugin"""
+    global server
+    print("loaded plugin")
+    #app.run(host = "0.0.0.0", port=8080)
+
+    print('starting server...')
+ 
+    # Server settings
+    # Choose port 8080, for port 80, which is normally used for a http server, you need root access
+    server_address = ('127.0.0.1', 8081)
+    server = HTTPServer(server_address, testHTTPServer_RequestHandler)
+    print('running server...')
+    Thread(target=start_server, args=[]).start()
+    #httpd.serve_forever()
+
+    """global SESSIONS, server
+
+    # Load settings
+    settings = sublime.load_settings("rsub.sublime-settings")
+    port = settings.get("port", 52698)
+    host = settings.get("host", "localhost")
+
+    # Start server thread
+    server = TCPServer((host, port), ConnectionHandler)
+    Thread(target=start_server, args=[]).start()
+    say('Server running on ' + host + ':' + str(port) + '...')"""
